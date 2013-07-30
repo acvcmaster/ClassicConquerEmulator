d34fcfd76ea29dd7566b6ceda29b8638578b83ec
acvcmaster <acvcnos@hotmail.com>
2013-07-29 21:07:25 -0300

    Event handlers + Debug junk added

--- CCO/CCO/Networking/Servers.cs
+++ CCO/CCO/Networking/Servers.cs
@@ -26,6 +26,27 @@ namespace CCO.Networking
             Listener = new HybridSocket(Port);
             Program.Report("Login server listening in all network interfaces on port " + Port + ".",
                 ConsoleColor.White, ReportType.Networking);
+            Listener.AnnounceNewConnection += Listener_AnnounceNewConnection;
+            Listener.AnnounceDisconnection += Listener_AnnounceDisconnection;
+            Listener.AnnounceReceive += Listener_AnnounceReceive;
+        }
+
+        void Listener_AnnounceReceive(byte[] arg1, nLink arg2, byte[] arg3)
+        {
+#if DEBUG
+            Program.Report("Data recieved!", ConsoleColor.Green, ReportType.Networking);
+#endif
+        }
+
+        void Listener_AnnounceDisconnection(nLink obj)
+        {
+        }
+
+        void Listener_AnnounceNewConnection(nLink obj)
+        {
+#if DEBUG
+            Program.Report("Connection recieved!", ConsoleColor.Green, ReportType.Networking);
+#endif
         }
     }
 }
